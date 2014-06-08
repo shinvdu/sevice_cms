754cf5ecea0ae860440a6681f15e7de74f0a5985
silas.xie <xie.tianbao44@gmail.com>
2014-06-08 23:24:38 +0800

    add __get & __set for content type

--- ContentType/ContentType.php
+++ ContentType/ContentType.php
@@ -1,10 +1,23 @@
 <?php
 abtract class ContentType
 {
+	protected $node = array();
+
 	__contruct($csv)
 	{
-		
+		$this->fromCsv($csv);
 	}
+
+	public function __get($key)
+	{
+		return $this->node[$key];
+	}
+
+	public function __set($key, $value)
+	{
+		$this->node[$key] = $value;
+	}
+
 	public function fromCsv($node)
 	{
 		# code...
