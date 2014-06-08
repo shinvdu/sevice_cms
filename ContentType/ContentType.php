7d02c6a2206a7bf25455264219d2a74f0c5641c4
tian <xie.tianbao44@gmail.com>
2014-06-08 12:40:52 +0800

    add abstrac content type

---
+++ ContentType/ContentType.php
@@ -0,0 +1,25 @@
+<?php
+abtract class ContentType
+{
+	__contruct($csv)
+	{
+		
+	}
+	public function fromCsv($node)
+	{
+		# code...
+	}
+	public function toCsv($node)
+	{
+		# code...
+	}
+	public function fromDrupal($node)
+	{
+		# code...
+	}
+	public function toDrupal($node)
+	{
+		# code...
+	}
+
+}
