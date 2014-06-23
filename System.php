75907488d22b78b8c7d116dcdc367db5480874cb
silas.xie <silas.xie@vidaxl.com>
2014-06-23 17:30:16 +0800

    add system variable surport

---
+++ System.php
@@ -0,0 +1,18 @@
+<?php
+
+class System extends Service {
+  public function __construct($options){
+    parent::__construct($options);
+    $this->connect();
+  }
+  public function variable_get($name, $value = '') {
+    return (object)$this->requestSend($this->_methods->SYSTEM_GET, array($name, $value));
+  }
+  public function variable_set($name, $value) {
+    return $this->requestSend($this->_methods->SYSTEM_SET, array($name, $value));
+  }
+
+  public function variable_delete($name) {
+    return $this->requestSend($this->_methods->SYSTEM_DELETE, array($name));
+  }
+}
