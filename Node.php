a5cd4a25f6201f5a194d1246c5c821295394a320
silas.xie <silas.xie@vidaxl.com>
2014-06-23 16:40:57 +0800

    add node create and delete

--- Node.php
+++ Node.php
@@ -4,22 +4,39 @@ class Node extends Service
 {
   // data stored in this object
   protected $node;
-
-  public function node_load($nid) {
+  
+  public function __construct($options){
+    parent::__construct($options);
     $this->connect();
+  }
+  public function node_load($nid) {
     return (object)$this->requestSend($this->_methods->NODE_GET, array($nid));
   }
+  public function node_create($node) {
+    if(is_array($node)){
+      $node = (object)$node;
+    }
+    return $this->requestSend($this->_methods->NODE_CREATE, array($node));
+  }
+
   public function node_save($nid, $node) {
-    $this->connect();
     if(is_object($node)){
       $node = (array)$node;
     }
     return $this->requestSend($this->_methods->NODE_SAVE, array($nid, $node));
   }
-
+  public function node_delete($nid) {
+   if (!is_array($nid)) {
+      $nid = (array)$nid;
+    } 
+    $return = array();
+    foreach ($$nid as $id) {
+      $return[] = $this->requestSend($this->_methods->NODE_DELETE, array($nid));
+    }
+    return $return;
+  }
   
   public function node_list(){
-    $this->connect();
       return $this->requestSend($this->_methods->NODE_LIST);
   }
 }
